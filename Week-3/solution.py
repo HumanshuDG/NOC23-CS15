c95a1206ccddcbf59f9fc2c5de8561fdd2959704
Humanshu D G <83282277+HumanshuDG@users.noreply.github.com>
2023-02-16 17:24:25 +0530

    Update solution.py

--- Week-3/solution.py
+++ Week-3/solution.py
@@ -1,13 +1,13 @@
 def remdup(l: list):
-	duplicate = []
-	for i in range(-1, -(len(l) + 1), -1):
-		if l[i] not in duplicate:
-			duplicate.append(l[i])
-	duplicate.reverse()
-	return duplicate
+    duplicate = []
+    for i in range(-1, -(len(l) + 1), -1):
+        if l[i] not in duplicate:
+            duplicate.append(l[i])
+    duplicate.reverse()
+    return duplicate
 
 def splitsum(l: list):
-	pass
+    pass
 
 def matrixflip(m, d):
-	pass
+    pass
